208752c8a33e8dd8dd03587fd56b9feec148cf07
Igor <iggronigor@gmail.com>
2026-09-11 11:41:21 -0300

    Add files via upload

---
+++ igor Jose 2ds/Atividades/atividades.php/empresa.php
@@ -0,0 +1,34 @@
+<?php
+
+$conexao = new mysqli("localhost", "root", "", "empresa");
+
+if ($conexao->connect_error) {
+    die("Erro na conexão de banco de dados");
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $matricula = $_POST['matricula'];
+    $funcao = $_POST['funcao'];
+    $departamento = $_POST['departamento'];
+    $idade = $_POST['idade'];
+    $cpf = $_POST['cpf'];
+    $rg = $_POST['rg'];
+    $salario = $_POST['salario'];
+    $endereco = $_POST['endereco'];
+    $uf = $_POST['uf'];
+    $pais = $_POST['pais']; 
+
+$sql = "INSERT INTO funcionario (nome, matricula, funcao, departamento, idade, cpf, rg, salario, endereco, uf, pais) 
+VALUES ('$nome', '$matricula', '$funcao', '$departamento', '$idade', '$cpf', '$rg', '$salario', '$endereco', '$uf', '$pais')";
+
+    if ($conexao->query($sql) == TRUE) {
+
+    echo "Funcionario cadastrado com sucesso.";    
+    } else {
+    echo "Erro ao cadastrar funcionario.";
+    }
+
+$conexao->close();
+}
+?>

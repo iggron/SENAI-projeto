633f0adc80ed60479068779144079f8fa9b445c9
Igor <iggronigor@gmail.com>
2026-09-11 11:00:52 -0300

    Minhas pastas
    Tem coisas como testes e atividades passadas

---
+++ igorjose2ds/CRUD/empresa.php
@@ -0,0 +1,35 @@
+
+<link rel="stylesheet" href="empresa.css">
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $conexao = new mysqli("localhost", "root", "Home@spSENAI2025!", "empresa");
+
+    if ($conexao->connect_error) {
+        die("Erro na conexão de banco de dados: " . $conexao->connect_error);
+    }
+
+    $nome = $_POST['nome']; 
+    $matricula = $_POST['matricula'];
+    $funcao = $_POST['funcao'];
+    $departamento = $_POST['departamento'];
+    $idade = $_POST['idade'];
+    $cpf = $_POST['cpf'];
+    $rg = $_POST['rg'];
+    $salario = $_POST['salario'];
+    $endereco = $_POST['endereco'];
+    $uf = $_POST['uf'];
+    $pais = $_POST['pais']; 
+
+$sql = "INSERT INTO funcionario (nome, matricula, funcao, departamento, idade, cpf, rg, salario, endereco, uf, pais) 
+VALUES ('$nome', '$matricula', '$funcao', '$departamento', '$idade', '$cpf', '$rg', '$salario', '$endereco', '$uf', '$pais')";
+
+    if ($conexao->query($sql) == TRUE) {
+
+    echo "Funcionario cadastrado com sucesso.";    
+    } else {
+    echo "Erro ao cadastrar funcionario.";
+    }
+
+  $conexao->close();
+}
+?>

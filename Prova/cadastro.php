a5dbb1de7429ecdaa54482efc78e2b8c403be922
Seu Nome <seu_email@exemplo.com>
2026-09-14 11:26:38 -0300

    php e html basico

--- Prova/cadastro.php
+++ Prova/cadastro.php
@@ -1,1 +1,14 @@
 <?php
+
+$conexao = new mysqli("localhost", "root", "Home@spSENAI2025!", "atividade");
+
+$nome = $_POST['nome'];
+$telefone = $_POST['telefone'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$sql = "insert into usuarios (nome, telefone, email, senha)
+values ('$nome', '$telefone', '$email', '$senha')";
+
+$conexao->query($sql);
+echo "Cadastro realizado com sucesso!";
